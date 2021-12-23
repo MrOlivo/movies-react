8b06ee94cd0d8a874fc2e940d6d4c95a4530ba7c
Olivo <46498127+MrOlivo@users.noreply.github.com>
2021-12-23 15:30:31 -0700

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,23 +1,16 @@
 import styles from './Search.module.css';
 import { FaSearch } from 'react-icons/fa';
-import { useEffect, useState } from 'react/cjs/react.development';
 import { useNavigate } from 'react-router-dom';
 import { useQuery } from '../hooks/useQuery';
 
 export function Search() {
-  const [searchText, setSearchText] = useState('');
-  const navigate = useNavigate();
-
   const query = useQuery();
   const search = query.get('search');
 
-  useEffect(() => {
-    setSearchText(search || '');
-  }, [search]);
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    navigate(`/?search=${searchText}`);
   };
 
   return (
@@ -26,8 +19,10 @@ export function Search() {
         <input
           className={styles.searchInput}
           type="text"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
+          value={search}
+          onChange={(e) => {
+            navigate(`/?search=${e.target.value}`);
+          }}
         />
         <button className={styles.searchButton} type="submit">
           <FaSearch size={20} />
