195fde1b249e6725d5a4958bc1f26ccfea505776
Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-02-05 18:11:58 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { App } from './App';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>,
+    <ReactQueryDevtools />
+  </QueryClientProvider>,
   document.getElementById('root')
 );
