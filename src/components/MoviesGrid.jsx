a0a787d880393bec50e6108ed712e5c5f3849d09
Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-02-05 18:49:53 -0700

    Update MoviesGrid.jsx

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,34 +1,13 @@
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { useInfiniteQuery } from 'react-query';
 
-import { get } from '../utils/httpClient';
+import { useMovies } from '../hooks/useMovies';
 import { Empty } from './Empty';
 import { MovieCard } from './MovieCard';
 import styles from './MoviesGrid.module.css';
 import { Spinner } from './Spinner';
 
 export function MoviesGrid({ search }) {
-  const { data, isLoading, hasNextPage, fetchNextPage } = useInfiniteQuery(
-    ['movies', search],
-    ({ pageParam = 1 }) => {
-      const searchUrl = search
-        ? `/search/movie?query=${search}&page=${pageParam}`
-        : `/discover/movie?page=${pageParam}`;
-      return get(searchUrl);
-    },
-    {
-      getNextPageParam: (lastPage) => {
-        if (lastPage.page === lastPage.total_pages) return false;
-        return lastPage.page + 1;
-      },
-    }
-  );
-
-  const movies =
-    data?.pages.reduce(
-      (prevMovies, page) => prevMovies.concat(page.results),
-      []
-    ) ?? [];
+  const { movies, isLoading, hasNextPage, fetchNextPage } = useMovies(search);
 
   if (!isLoading && movies.length === 0) {
     return <Empty />;
