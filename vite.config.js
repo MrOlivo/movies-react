f2f9aa4feb1c30aa1f9ea36ebe1f6a3a1a08b513
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2024-02-20 16:03:47 -0700

    Add tsconfigPaths plugin to Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/movies-react/',
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
