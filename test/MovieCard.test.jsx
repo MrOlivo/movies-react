4e68bd40b90a03b4cdceb3ee2a59de8a8d43e1a1
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2024-02-24 10:36:40 -0700

    Update biome.json and test files formatting

--- test/MovieCard.test.jsx
+++ test/MovieCard.test.jsx
@@ -1,53 +1,56 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { describe, expect, test } from "vitest";
-import { MovieCard } from "../src/components/MovieCard";
+import { render, screen } from "@testing-library/react"
+import { BrowserRouter as Router } from "react-router-dom"
+import { describe, expect, test } from "vitest"
+import { MovieCard } from "../src/components/MovieCard"
 
-describe('MovieCard', () => {
+describe("MovieCard", () => {
   const mockMovie = {
     id: 1,
-    title: 'Test Movie',
-    poster_path: '/test.jpg',
+    title: "Test Movie",
+    poster_path: "/test.jpg",
     year: 2022,
-  };
+  }
 
-  test('renders without crashing', () => {
+  test("renders without crashing", () => {
     render(
       <Router>
         <MovieCard movie={mockMovie} />
-      </Router>
-    );
-  });
+      </Router>,
+    )
+  })
 
-  test('displays the movie title', () => {
+  test("displays the movie title", () => {
     render(
       <Router>
         <MovieCard movie={mockMovie} />
-      </Router>
-    );
+      </Router>,
+    )
 
     expect(screen.getByText(/Test Movie/i)).toBeDefined()
-  });
+  })
 
-  test('displays the movie image', () => {
+  test("displays the movie image", () => {
     render(
       <Router>
         <MovieCard movie={mockMovie} />
-      </Router>
-    );
-    
-    const img = screen.getByRole('img');
-    expect(img).toHaveProperty('src', expect.stringContaining('/test.jpg'));
-  });
+      </Router>,
+    )
 
-  test('links to the correct movie detail page', () => {
+    const img = screen.getByRole("img")
+    expect(img).toHaveProperty("src", expect.stringContaining("/test.jpg"))
+  })
+
+  test("links to the correct movie detail page", () => {
     render(
       <Router>
         <MovieCard movie={mockMovie} />
-      </Router>
-    );
-    
-    const link = screen.getByRole('link');
-    expect(link).toHaveProperty('href', expect.stringContaining('/movies-react/movie/1'));
-  });
-});
+      </Router>,
+    )
+
+    const link = screen.getByRole("link")
+    expect(link).toHaveProperty(
+      "href",
+      expect.stringContaining("/movies-react/movie/1"),
+    )
+  })
+})
--- test/MoviesGrid.test.jsx
+++ test/MoviesGrid.test.jsx
@@ -1,75 +1,74 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import { describe, expect, test } from "vitest";
-import { BrowserRouter as Router } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { MoviesGrid } from "../src/components/MoviesGrid";
+import { render, screen, waitFor } from "@testing-library/react"
+import { QueryClient, QueryClientProvider } from "react-query"
+import { BrowserRouter as Router } from "react-router-dom"
+import { describe, expect, test } from "vitest"
+import { MoviesGrid } from "../src/components/MoviesGrid"
 
-describe('MoviesGrid', () => {
-
+describe("MoviesGrid", () => {
   const queryClient = new QueryClient()
 
   const mockMovies = [
     {
       id: 447404,
-      title: 'Pokémon Detective Pikachu',
-      poster_path: '/uhWvnFgg3BNlcUz0Re1HfQqIcCD.jpg',
+      title: "Pokémon Detective Pikachu",
+      poster_path: "/uhWvnFgg3BNlcUz0Re1HfQqIcCD.jpg",
       year: 2019,
     },
     {
       id: 436931,
-      title: 'Pokémon the Movie: I Choose You!',
-      poster_path: '/cmpOaiRtjnhLNF2iPslMXCOcVJ.jpg',
+      title: "Pokémon the Movie: I Choose You!",
+      poster_path: "/cmpOaiRtjnhLNF2iPslMXCOcVJ.jpg",
       year: 2017,
     },
-  ];
+  ]
 
-  test('renders without crashing', () => {
+  test("renders without crashing", () => {
     render(
       <QueryClientProvider client={queryClient}>
         <MoviesGrid search="test" />
-      </QueryClientProvider>
-    );
-  });
+      </QueryClientProvider>,
+    )
+  })
 
-  test('displays the movie cards', async () => {
+  test("displays the movie cards", async () => {
     render(
       <QueryClientProvider client={queryClient}>
         <Router>
           <MoviesGrid search="pokemon" />
         </Router>
-      </QueryClientProvider>
-    );
+      </QueryClientProvider>,
+    )
 
     await waitFor(() => {
-      mockMovies.forEach(movie => {
+      mockMovies.map((movie) => {
         const titleRgx = new RegExp(movie.title)
-        expect(screen.getByText(titleRgx)).toBeDefined();
-      });
+        expect(screen.getByText(titleRgx)).toBeDefined()
+      })
     })
-  });
+  })
 
-  test('displays the spinner when loading', () => {
+  test("displays the spinner when loading", () => {
     render(
       <QueryClientProvider client={queryClient}>
         <MoviesGrid search="test" />
-      </QueryClientProvider>
-    );
+      </QueryClientProvider>,
+    )
 
-    expect(screen.getByTestId('spinner')).toBeDefined();
-  });
+    expect(screen.getByTestId("spinner")).toBeDefined()
+  })
 
-  test('displays the empty message when no movies are found', async () => {
+  test("displays the empty message when no movies are found", async () => {
     render(
       <QueryClientProvider client={queryClient}>
         <Router>
           <MoviesGrid search="displays the empty message when no movies are found" />
         </Router>
-      </QueryClientProvider>
-    );
+      </QueryClientProvider>,
+    )
 
-    const resultsRgx = new RegExp('No Results')
+    const resultsRgx = /No Results/
     await waitFor(() => {
-      expect(screen.getByText(resultsRgx)).toBeDefined();
+      expect(screen.getByText(resultsRgx)).toBeDefined()
     })
-  });
-});
+  })
+})
